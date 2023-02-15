b951ef14e517164dcb472e2bb4cf94ec5289502f
Autokaka <qq1909698494@gmail.com>
2023-02-15 09:23:02 +0800

    chore: update example code style

--- example/darwin/ContentView.swift
+++ example/darwin/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ContentViewPreviewProvider: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
